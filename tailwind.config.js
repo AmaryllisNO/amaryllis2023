3b0d730e310eaa855f200c632e689f2aa3f45ffb
Alexis <amaryllisno@gmail.com>
2023-07-27 19:53:15 +0200

    tailwind configuration setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
+    letterSpacing: {
+      widest: '.25em',
+    },
   },
-  plugins: [],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
